61fcaa47c12e489d77931e4fd6a4d3758204039e
zezhi zheng <zezhiz@cs.washington.edu>
2017-01-18 14:45:39 -0800

    header constrcution done

--- project1/project1_1.py
+++ project1/project1_1.py
@@ -1,20 +1,38 @@
 import sys
 import socket
-import numpy
+import numpy as py
 
 #np.uint32()
 
 std_id = 412
 
 def construct_header(payload_len, psecret, step, last_digit):
-	return str(payload_len) + str(psecret) + str(step) + str(last_digit)
+	header = []
 
-def construct_packet(header, payload, payload_len):
-	return str(header) + str(payload)
+	for x in range (0, 2):
+		header.append(last_digit % 255)
+		last_digit = last_digit // 255
+
+	for x in range (0, 2):
+		header.append(step // 255)
+		step = step % 255
+
+	for x in range (0, 4):
+		header.append(psecret % 255)
+		psecret = psecret // 255
+
+	for x in range (0, 4):
+		header.append(payload_len % 255)
+		payload_len = payload_len // 255
+
+	return header[::-1]
+
+def construct_packet(header, payload):
+	return bytes(header) + payload.encode()
 
 def packer(payload):
-	pad = 4 - (sys.getsizeof(payload) % 4)
-	return "\0" * pad + payload
+	pad = 4 - (len(payload) % 4)
+	return payload.zfill(pad + len(payload))
 
 HOST = 'attu2.cs.washington.edu'
 PORT = 12235
@@ -22,22 +40,39 @@ PORT = 12235
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect((HOST, PORT))
 
-#stage a
-print ('stage a1')
-payload = packer('hello world')
-print ('the payload is: ', payload)
-	
-header = construct_header(str(sys.getsizeof(payload)),
-						  numpy.uint32(0),
-						  numpy.uint16(1),
-						  numpy.uint16(std_id))
-packet = construct_packet(header, payload, str(sys.getsizeof(payload)))
+print('aligning payload......')
+payload = packer('hello world\0')
+print('payload aligning done, constructing the header......')
+header = construct_header(len(payload), 0, 1, std_id)
+print('header construction done, preparing the packet......')
+packet = construct_packet(header, payload)
+print('packet ready\n')
+print(packet)
 
-print ('finished packing')
-print (packet.encode())
-s.sendall(packet.encode())
+s.sendall(packet)
+print ('packet sent')
 data = s.recv(1024)
+print ('packet received')
 s.close()
+
+#stage a
+# print ('stage a1')
+# payload = packer('hello world')
+# print ('the payload is: ', payload)
+	
+# header = construct_header(str(sys.getsizeof(payload)),
+# 						  numpy.uint32(0),
+# 						  numpy.uint16(1),
+# 						  numpy.uint16(std_id))
+# packet = construct_packet(header, payload, str(sys.getsizeof(payload)))
+
+# print ('finished packing')
+# print (packet.encode())
+# s.sendall(packet.encode())
+# print ('packet sent')
+# data = s.recv(1024)
+# print ('packet received')
+# s.close()
 
 #stage b
 #print 'stage b1'
